dce0597fbb83a8c35b536f76a518f608e37ca04d
DanielVargas99 <daniel.vargas@correounivalle.edu.co>
2022-10-04 10:45:32 -0500

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,96 @@
+const path = require('path');
+
+// PLUGINS Y MINIFICADORES DE CSS Y SCSS/SASS
+// Para reducir el tamaño de las hojas de estilo de nuestro proyecto
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Para el template del HTML que va a usar Webpack
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Para reducir los CSS
+const { SourceMapDevToolPlugin } = require('webpack'); // Para conocer el Source Map de nuestro proyecto
+const ESLintWebpackPlugin = require('eslint-webpack-plugin');
+
+// Configuraciones del puerto
+const port = process.env.PORT || 3000;
+
+// Exportar configuración de Webpack
+module.exports = {
+    entry: './src/index.jsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.[hash].js',
+        publicPath: '/',
+    },
+    context: path.resolve(__dirname),
+    devServer: {
+        port,
+        historyApiFallback: true,
+    },
+    devtool: 'eval-source-map',
+    module: {
+        rules: [
+            // Reglas para archivos JS y JSX
+            // Reglas de Babel ES y JSX
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            '@babel/env',
+                            '@babel/react',
+                          ],
+                    },
+                },
+            },
+            // Reglas para archivos CSS, SASS, SCSS para minificarlos y cargarlos en el bundle
+            {
+                test: /(\.css|\.scss|\.sass)$/,
+                use: [
+                    { loader: MiniCssExtractPlugin.loader },
+                    { loader: 'css-loader' },
+                    { loader: 'sass-loader' },
+                ],
+            },
+            // Reglas para los archivos de imágenes
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        // Template HTML
+        new HtmlWebpackPlugin(
+            {
+                template: './public/index.html',
+            },
+        ),
+        new MiniCssExtractPlugin(
+            {
+                filename: './css/styles.css',
+            },
+        ),
+        new SourceMapDevToolPlugin(
+            {
+                filename: '[file].map',
+            },
+        ),
+        new ESLintWebpackPlugin(
+            {
+                extensions: ['js', 'jsx'],
+            },
+        ),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx', '.css', '.scss', '.sass'],
+        modules: [
+            'node_modules',
+        ],
+        alias: {
+          'react-redux': path.join(__dirname, '/node_modules/react-redux/dist/react-redux.min'),
+        },
+    },
+};
